07c2ed97517a28b79202bd09c62cbc74053cb8c7
Brandon Stultz <infinitenerd@infinitenode.com>
2012-08-05 02:08:51 -0400

    multiline functionality operational

--- file_search.c
+++ file_search.c
@@ -117,20 +117,19 @@ int main(int argc, char **argv) {
   }
 
   #ifdef DEBUG
-  printf("Match succeeded at offset %d\n", pcre_info->ovector[0]);
+    printf("Match succeeded at offset %d\n", pcre_info->ovector[0]);
 
-  // Show substrings stored in output vector
-  int i;
-  for(i=0;i<(pcre_info->rc);i++)
-  {
-    char *substring_start = pcre_info->buffer + pcre_info->ovector[2*i];
-    int substring_length = pcre_info->ovector[2*i+1] - pcre_info->ovector[2*i];
-    printf("%2d: %.*s\n", i, substring_length, substring_start);
-  }
+    // Show substrings stored in output vector
+    int i;
+    for(i=0;i<(pcre_info->rc);i++)
+    {
+      char *substring_start = pcre_info->buffer + pcre_info->ovector[2*i];
+      int substring_length = pcre_info->ovector[2*i+1] - pcre_info->ovector[2*i];
+      printf("%2d: %.*s\n", i, substring_length, substring_start);
+    }
   #endif
 
   //XXX get_named_substring
-
   const char *named_substring=argv[3];
   const char *matched_substring = NULL;
 
@@ -143,8 +142,156 @@ int main(int argc, char **argv) {
   printf("substring match for %s: %s\n",named_substring,matched_substring);
 
   pcre_free_substring(matched_substring);
-
   //XXX
+
+  //XXX search for additonal matches
+
+  // do we need this? -->
+  int utf8;
+  unsigned int option_bits;
+  (void)pcre_fullinfo(pcre_info->re,NULL,PCRE_INFO_OPTIONS,&option_bits);
+  utf8 = option_bits & PCRE_UTF8;
+  option_bits &= PCRE_NEWLINE_CR|PCRE_NEWLINE_LF|PCRE_NEWLINE_CRLF|
+                 PCRE_NEWLINE_ANY|PCRE_NEWLINE_ANYCRLF;
+
+  if(option_bits==0)
+  {
+    int d;
+    (void)pcre_config(PCRE_CONFIG_NEWLINE,&d);
+    option_bits = (d == 13)? PCRE_NEWLINE_CR :
+                  (d == 10)? PCRE_NEWLINE_LF :
+                  (d == (13<<8 | 10))? PCRE_NEWLINE_CRLF :
+                  (d == -2)? PCRE_NEWLINE_ANYCRLF :
+                  (d == -1)? PCRE_NEWLINE_ANY : 0;
+  }
+
+  int crlf_is_newline;
+  crlf_is_newline =
+       option_bits == PCRE_NEWLINE_ANY ||
+       option_bits == PCRE_NEWLINE_CRLF ||
+       option_bits == PCRE_NEWLINE_ANYCRLF;
+  // <-- do we need this?
+
+  // loop for second and subsequent matches
+
+  for(;;)
+  {
+    int options = 0;
+    int start_offset = pcre_info->ovector[1];
+
+    if(pcre_info->ovector[0] == pcre_info->ovector[1])
+    {
+      if(pcre_info->ovector[0] == pcre_info->buffer_length) break;
+      options = PCRE_NOTEMPTY_ATSTART | PCRE_ANCHORED;
+    }
+
+    // run the next matching operation
+    pcre_info->rc = pcre_exec(
+      pcre_info->re,
+      NULL,
+      pcre_info->buffer,
+      pcre_info->buffer_length,
+      start_offset,
+      options,
+      pcre_info->ovector,
+      OVECCOUNT);
+
+    if(pcre_info->rc == PCRE_ERROR_NOMATCH)
+    {
+      if(options == 0) break;
+      pcre_info->ovector[1] = start_offset + 1; //advance one byte
+      if(crlf_is_newline &&
+         start_offset < pcre_info->buffer_length - 1 &&
+         pcre_info->buffer[start_offset] == '\r' &&
+         pcre_info->buffer[start_offset + 1] == '\n')
+      {
+        pcre_info->ovector[1] += 1;
+      } else if(utf8) {
+        while(pcre_info->ovector[1] < pcre_info->buffer_length)
+        {
+         if((pcre_info->buffer[pcre_info->ovector[1]] & 0xc0) != 0x80) break;
+         pcre_info->ovector[1] += 1;
+        }
+      }
+      continue;
+    }
+
+    if(pcre_info->rc < 0)
+    {
+      printf("Matching error %d\n", pcre_info->rc);
+      pcre_free(pcre_info->re);
+      return 1;
+    }
+
+    //printf("\nMatch succeeded again at offset %d\n", pcre_info->ovector[0]);
+
+    if(pcre_info->rc==0)
+    {
+      pcre_info->rc = OVECCOUNT/3;
+      printf("ovector only has room for %d captured substrings\n", pcre_info->rc -1);
+    }
+
+    /* As before, show substrings stored in the output vector by number, and then
+       also any named substrings. */
+
+    #ifdef DEBUG
+      int i;
+      for (i = 0; i < pcre_info->rc; i++)
+      {
+        char *substring_start = pcre_info->buffer + pcre_info->ovector[2*i];
+        int substring_length = pcre_info->ovector[2*i+1] - pcre_info->ovector[2*i];
+        printf("%2d: %.*s\n", i, substring_length, substring_start);
+      }
+    #endif
+
+    int namecount;
+
+    (void)pcre_fullinfo(
+    pcre_info->re,
+    NULL,
+    PCRE_INFO_NAMECOUNT,
+    &namecount);
+
+    if (namecount > 0)
+    {
+
+      if((fetch_named_substring(named_substring, pcre_info, &matched_substring)) < 0)
+      {
+        fprintf(stderr,"error: named substring %s not found\n",named_substring);
+        return 1;
+      }
+      printf("substring match for %s: %s\n",named_substring,matched_substring);
+      pcre_free_substring(matched_substring);
+
+      #ifdef DEBUG
+        int name_entry_size;
+
+        (void)pcre_fullinfo(
+        pcre_info->re,            /* the compiled pattern */
+        NULL,                     /* no extra data - we didn't study the pattern */
+        PCRE_INFO_NAMEENTRYSIZE,  /* size of each entry in the table */
+        &name_entry_size);        /* where to put the answer */
+        unsigned char *name_table;
+
+        (void)pcre_fullinfo(
+        pcre_info->re,            /* the compiled pattern */
+        NULL,                     /* no extra data - we didn't study the pattern */
+        PCRE_INFO_NAMETABLE,      /* address of the table */
+        &name_table);             /* where to put the answer */
+
+        unsigned char *tabptr = name_table;
+        printf("Named substrings\n");
+        for (i = 0; i < namecount; i++)
+        {
+          int n = (tabptr[0] << 8) | tabptr[1];
+          printf("(%d) %*s: %.*s\n", n, name_entry_size - 3, tabptr + 2,
+            pcre_info->ovector[2*n+1] - pcre_info->ovector[2*n], pcre_info->buffer + pcre_info->ovector[2*n]);
+          tabptr += name_entry_size;
+        }
+      #endif
+    }
+  }      /* End of loop to find second and subsequent matches */
+
 
   // cleanup
   free(pcre_info->buffer);
