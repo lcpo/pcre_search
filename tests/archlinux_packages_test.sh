e8b3d42625df8006c67e66f7a4c393f4b377fcc1
Brandon Stultz <infinitenerd@infinitenode.com>
2012-08-27 02:52:19 -0400

    fix test path

--- tests/archlinux_packages_test.sh
+++ tests/archlinux_packages_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./curl_search "http://www.archlinux.org/feeds/packages/" "<title>(?P<title>[^<]+)</title>" title
+../curl_search "http://www.archlinux.org/feeds/packages/" "<title>(?P<title>[^<]+)</title>" title
