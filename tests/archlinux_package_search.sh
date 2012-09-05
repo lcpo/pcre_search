0e496172fb604447be30242d94d92ecddf10f85b
Brandon Stultz <infinitenerd@infinitenode.com>
2012-09-05 17:36:20 -0400

    testing

---
+++ tests/archlinux_package_search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! "$1" ]; then
+  echo "Usage: $0 [query]"
+  exit 1
+fi
+
+echo "searching for: $1"
+
+../curl_search "http://www.archlinux.org/packages/?q=$1" \
+               "<a\s[^>]+?title=\"View package details for[^>]+?>(?P<title>[a-zA-Z\-\_]+)</a></td><td>[^<]+?</td><td\sclass=\"wrap\">(?P<description>[^<]+)</td>" \
+               description
--- tests/google_web_search.sh
+++ tests/google_web_search.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-../curl_search "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=github" "titleNoFormatting\":\"(?P<title>[^\"]+)\"" title
+if [ ! "$1" ]; then
+  echo "Usage: $0 [query]"
+  exit 1
+fi
+
+echo "searching for: $1"
+
+../curl_search "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=$1" "titleNoFormatting\":\"(?P<title>[^\"]+)\"" title
