18aae5139ef93467122223a743019608c72a84e4
Brandon Stultz <infinitenerd@infinitenode.com>
2012-10-03 23:52:18 -0400

    ifl is now much cleaner

--- ifl.c
+++ ifl.c
@@ -6,28 +6,6 @@
 
 #define WEB_API "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="
 
-void pcre_match_callback(PCRE_CONTAINER *pcre_info)
-{
-  // get_named_substring if it exists
-  if(pcre_info->namecount > 0)
-  {
-    const char *matched_substring = NULL;
-
-    if((fetch_named_substring(pcre_info->named_substring, pcre_info, &matched_substring)) >= 0)
-    {   
-      char *ret = NULL;
-      if((ret=str_replace("\\u0026#39;","\'",(char*)matched_substring)))
-      {   
-        printf("%s\n",ret);
-        free(ret);
-      } else {
-        printf("%s\n",matched_substring);
-      }   
-      pcre_free_substring(matched_substring);
-    }   
-  }
-}
-
 int main(int argc, char **argv)
 {
   // check cli args
@@ -46,44 +24,16 @@ int main(int argc, char **argv)
     sprintf(url, "%s+%s", url, argv[i]);
   }
 
-  //printf("url: %s\n",url);
+  list_t *list;
 
-  CURL_BUFFER *curl_buffer = request(url);
-  if(!curl_buffer)
-    return 1;
+  list = curl_pcre_search(url, "unescapedUrl\":\"(?P<url>[^\"]+)\".*?titleNoFormatting\":\"(?P<title>[^\"]+)\"",
+                          "url", "title");
 
-  PCRE_CONTAINER *pcre_info = pcre_container_new();
-  if(!pcre_info)
-  {
-    fprintf(stderr,"Error: malloc() pcre_container\n");
-    curl_buffer_delete(curl_buffer);
-    return 1;
-  }
+  ll_puts(list, "url");
+  ll_puts(list, "title");
 
-  pcre_info->buffer = curl_buffer->memory;
-  pcre_info->buffer_length = curl_buffer->size;
-  pcre_info->pattern = "titleNoFormatting\":\"(?P<title>[^\"]+)\"";
-  pcre_info->named_substring = "title";
+  list_del(list);
+  free(url);
 
-  if(pcre_exec_single(pcre_info,pcre_match_callback))
-  {
-    curl_buffer_delete(curl_buffer);
-    pcre_container_delete(pcre_info);
-    return 1;
-  }
-
-  pcre_info->pattern = "unescapedUrl\":\"(?P<url>[^\"]+)\"";
-  pcre_info->named_substring = "url";
-
-  if(pcre_exec_single(pcre_info,pcre_match_callback))
-  {
-    curl_buffer_delete(curl_buffer);
-    pcre_container_delete(pcre_info);
-    return 1;
-  }
-
-  // cleanup and return normal
-  curl_buffer_delete(curl_buffer);
-  pcre_container_delete(pcre_info);
   return 0;
 }
